b8cc1aeb36a104d7416e7a3afaa334b5455ab814
Everton Souza <evrasouza@gmail.com>
2022-02-09 08:20:28 -0300

    first commit

---
+++ src/test/java/modulos/Usuarios/Usuarios.java
@@ -0,0 +1,62 @@
+package modulos.Usuarios;
+
+import static io.restassured.RestAssured.basePath;
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import org.junit.Test;
+
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+
+
+public class Usuarios {
+	
+	@Test
+	public void testListarUsuariosCadastrados() {
+		
+		// Configurando os dados basicos da requisicao
+		baseURI = "https://serverest.dev";
+		basePath = "/usuarios";
+
+		System.out.println("======================INICIO DOS REQUESTS======================");
+		given()
+			//.log().all()
+			//.filter(new RequestLoggingFilter())
+			.filter(new ResponseLoggingFilter())
+		.when()
+			.get()
+		.then()
+			//.log().all()
+			.statusCode(200)
+			.body("quantidade", equalTo(354));
+		
+		System.out.println("======================FIM DOS REQUESTS======================");
+	}
+	
+	
+	@Test
+	public void testListarUsuariosCadastradosPorID() {
+		
+		// Configurando os dados basicos da requisicao
+		baseURI = "https://serverest.dev";
+		basePath = "/usuarios";
+
+		System.out.println("======================INICIO DOS REQUESTS======================");
+		given()
+			//.log().all()
+			.filter(new RequestLoggingFilter())
+			.filter(new ResponseLoggingFilter())
+		.when()
+			.get("/0DlgwB2rgxJPsn7r")
+		.then()
+			//.log().all()
+			.statusCode(200)
+			.body("password", equalTo("teste"))
+			.body("administrador", equalTo("false"));
+		
+		System.out.println("======================FIM DOS REQUESTS======================");
+	}
+
+}
