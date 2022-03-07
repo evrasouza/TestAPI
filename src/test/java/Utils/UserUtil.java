7b8fab416c682b53d3ccbd9bf63567004329c317
Everton Souza <evrasouza@gmail.com>
2022-03-06 21:26:32 -0300

    ListUserTest

--- src/test/java/Utils/UserUtil.java
+++ src/test/java/Utils/UserUtil.java
@@ -1,6 +1,7 @@
 package Utils;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 import Core.BaseTest;
 import Factory.UserDataFactory;
@@ -9,6 +10,8 @@ import POJO.UsersPojo;
 public class UserUtil extends BaseTest{
 	
 	public String id;
+	public int amount;
+	public String name;
 	
     public String createUserId(){
     	UsersPojo user = new UserDataFactory().userAdmin();	
@@ -20,5 +23,30 @@ public class UserUtil extends BaseTest{
                 .extract().path("_id");
         return id;
     }
-
+    
+    public int amountUsers(){
+    	amount = given()
+    			.when()
+                	.get(APP_BASE_URL_USUARIOS)
+                .then()
+        			.statusCode(200)
+        				.extract().path("quantidade");
+        return amount;
+    }
+    
+    public String nameUser(){
+    	id = new UserUtil().createUserId();
+    	
+    	name = given()
+    			.pathParam("_id", id)
+    		.when()
+    			.get("/usuarios/{_id}")
+    		.then()
+    			.statusCode(200)
+	    			.body("password", equalTo("teste"))
+	    			.body("administrador", equalTo("true"))
+	    				.extract().path("nome");
+    	System.out.println(name);
+        return name;
+    }
 }
